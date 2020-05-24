8b184968a37b23eea0e2c45af841314c771dbc3e
ouaked <40290234+ouaked@users.noreply.github.com>
2020-05-24 12:18:36 +0200

    Add files via upload

---
+++ Hangar.java
@@ -0,0 +1,11 @@
+public class Hangar {
+
+	public static void main (String[] args) {
+		Boat  hamid = new Boat("hamid", 800);
+		Car ford = new Car("ford", 100000);	
+		
+		hamid.doStuff();
+		ford.doStuff();
+
+	}
+}
---
+++ Vehicle.java
@@ -0,0 +1,37 @@
+package autoboat01;
+
+public abstract class Vehicle {
+
+	// attributs
+	private String brand;
+	private int kilometers;
+
+	// constructeurs
+	public Vehicle(String brand, int kilometers) {
+		this.brand = brand;
+		this.kilometers = kilometers;
+	}
+
+	// méthode
+	public abstract String doStuff();
+
+	// Getters
+	public String getBrand() {
+		return this.brand;
+	}
+
+	public int getKilometers() {
+		return this.kilometers;
+	}
+
+	// setters
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public void setKilometers(int kilometers) {
+		this.kilometers = kilometers;
+	}
+
+
+}
